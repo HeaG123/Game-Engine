2f76e76dc578b1b0258fafa86f98484d8a1f0eab
Tom <tomn505@gmail.com>
2022-02-27 19:55:41 +0200

    original name here

--- src/main/java/com/tomn/java/game/engine/renderer/SpriteRenderer.java
+++ src/main/java/com/tomn/java/game/engine/renderer/SpriteRenderer.java
@@ -1,14 +1,23 @@
 package com.tomn.java.game.engine.renderer;
 
 import com.tomn.java.game.engine.Component;
+import org.joml.Vector2f;
 import org.joml.Vector4f;
 
 public class SpriteRenderer extends Component {
 
     private Vector4f color;
+    private Vector2f texCords;
+    private Texture texture;
 
     public SpriteRenderer(Vector4f color) {
         this.color = color;
+        this.texCords = null;
+    }
+
+    public SpriteRenderer(Texture texture) {
+        this.texture = texture;
+        this.color = new Vector4f(1,1,1,1);
     }
 
     @Override
@@ -23,5 +32,20 @@ public class SpriteRenderer extends Component {
 
     public Vector4f getColor() {
         return this.color;
+    }
+
+    public Texture getTexture() {
+        return this.texture;
+    }
+
+    public Vector2f[] getTexCords() {
+        Vector2f[] texCoord = {
+                new Vector2f(1,1),
+                new Vector2f(1,0),
+                 new Vector2f(0,0),
+                new Vector2f(0,1)
+        };
+
+        return texCoord;
     }
 }
